22c1945fdc23fbd0ba6edf09375f8b33d9c375e2
rodbnegri <rodolfobnegri@yahoo.com.br>
2025-01-08 15:21:04 -0300

    adding dependencies

--- src/SmallBodyGravity.jl
+++ src/SmallBodyGravity.jl
@@ -1,19 +1,17 @@
 module SmallBodyGravity
 
-using Pkg
-Pkg.add("Printf")
-
+using Printf
+using LinearAlgebra, DelimitedFiles
 
 include("spherical_harmonics.jl")
 include("polyhedron_model.jl")
 include("polyhedron_correction.jl")
 include("create_edge_coordinates.jl")
 
-export spherical_harmonics 
+export spherical_harmonics
 export polyhedron_model
 export polyhedron_correction
 export create_edge_coordinates
 
 end
-
 
