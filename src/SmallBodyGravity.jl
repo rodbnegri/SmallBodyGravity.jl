b18ed8e2c0d747d3fe83b40029f4881bf9d30aa6
rodbnegri <rodolfobnegri@yahoo.com.br>
2025-01-13 15:10:29 -0300

    process_polyhedron function

--- src/SmallBodyGravity.jl
+++ src/SmallBodyGravity.jl
@@ -4,12 +4,13 @@ using Printf
 using DelimitedFiles
 using LinearAlgebra
 
+include("process_polyhedron.jl")
 include("spherical_harmonics.jl")
 include("polyhedron_model.jl")
 include("polyhedron_correction.jl")
 include("create_edge_coordinates.jl")
 include("polyhedron_data.jl")
-include("process_polyhedron.jl")
+
 
 export spherical_harmonics
 export polyhedron_model
